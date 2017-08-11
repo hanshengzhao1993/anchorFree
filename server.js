6a67696e9daa7178c38b485775adab88cae08338
Han <hanshengzhao1993@gmail.com>
2017-08-11 14:31:08 -0700

    took out uneeded files

--- server.js
+++ server.js
@@ -4,19 +4,9 @@ const fs = require('fs');
 
 const app = express();
 
-// // parse application/x-www-form-urlencoded 
-// app.use(bodyParser.urlencoded({ extended: false }))
- 
-// // parse application/json 
-// app.use(bodyParser.json());
-
 // path to static files
 app.use(express.static("public"));
 
-// WildCard for all other routes
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./public/main.html"));
-// });
 
 //MAIN:
 app.listen(3000, function () {
