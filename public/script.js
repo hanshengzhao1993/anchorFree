fcc3e4774e2fa4420be56b3a1ee2094126be0a78
Han <hanshengzhao1993@gmail.com>
2017-08-11 13:50:16 -0700

    added ability to change pages

--- public/script.js
+++ public/script.js
@@ -1,3 +1,76 @@
-window.onload = function () {
-  console.log('hi')
+'use strict';
+window.onload = ()=> {
+
+  function hasClass(ele,cls) {
+    return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+  };
+
+  function removeClass(ele,cls) {
+    if (hasClass(ele,cls)) {
+      var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+      ele.className=ele.className.replace(reg,' ');
+    }
+  };
+
+  let headerButtons = document.getElementById('header');
+  headerButtons.addEventListener('click', (event)=>{
+    event.preventDefault();
+    if(event.srcElement.tagName === 'A'){
+      let articleNumber = event.srcElement.innerHTML.slice(5);
+      let articleNames = document.getElementsByTagName('article');
+      for(let i = 0; i< articleNames.length; i++){
+        if(articleNames[i].id.includes(articleNumber)){
+          // should take out no-show if there is a class
+          // if it is current show dont go anything
+          if(articleNames[i].className.includes('show')){
+            // first check if element is currently being showed
+            // dont do anything
+            console.log('clicked on the same as ths showing')
+          } else {
+            console.log('clicked now the same as showing')
+            for(let j = 0; j< articleNames.length; j++){
+              if(articleNames[j].className.includes('show')){
+                removeClass(articleNames[j], 'show');
+                articleNames[j].className += ' hide';
+              }
+            }
+            removeClass(articleNames[i], 'hide');
+            articleNames[i].className += ' show';
+          }
+        }
+      }
+    } 
+  })
+
+  let NavButtons = document.getElementById('nav-buttons');
+  NavButtons.addEventListener('click', (event)=>{
+    event.preventDefault();
+    if(event.srcElement.tagName === 'A'){
+      let articleNumber = event.srcElement.innerHTML.slice(5);
+      let articleNames = document.getElementsByTagName('article');
+      for(let i = 0; i< articleNames.length; i++){
+        if(articleNames[i].id.includes(articleNumber)){
+          // should take out no-show if there is a class
+          // if it is current show dont go anything
+          if(articleNames[i].className.includes('show')){
+            // first check if element is currently being showed
+            // dont do anything
+            console.log('clicked on the same as ths showing')
+          } else {
+            console.log('clicked now the same as showing')
+            for(let j = 0; j< articleNames.length; j++){
+              if(articleNames[j].className.includes('show')){
+                removeClass(articleNames[j], 'show');
+                articleNames[j].className += ' hide';
+              }
+            }
+            removeClass(articleNames[i], 'hide');
+            articleNames[i].className += ' show';
+          }
+        }
+      }
+    } 
+  })
+
+
 }
