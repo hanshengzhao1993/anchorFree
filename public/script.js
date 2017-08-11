084c240b34310e1fc1dd6de27b9d769f45526914
Han <hanshengzhao1993@gmail.com>
2017-08-11 14:32:28 -0700

    added some logic with buttons

--- public/script.js
+++ public/script.js
@@ -1,10 +1,12 @@
 'use strict';
 window.onload = ()=> {
 
+  // Used to check if it has class
   function hasClass(ele,cls) {
     return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
   };
 
+  // used to remove class
   function removeClass(ele,cls) {
     if (hasClass(ele,cls)) {
       var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
@@ -12,6 +14,7 @@ window.onload = ()=> {
     }
   };
 
+  // header buttons click event
   let headerButtons = document.getElementById('header');
   headerButtons.addEventListener('click', (event)=>{
     event.preventDefault();
@@ -39,15 +42,45 @@ window.onload = ()=> {
           }
         }
       }
+      
+      for(let j = 0; j< articleNames.length; j++){
+        if(articleNames[j].className.includes('show')){
+          let navButtonNumber = j + 1;
+          // then find the nav li that has that navButtonNumber
+          for(let l = 0; l< navButtons.childNodes.length; l++){
+            if(navButtons.childNodes[l].tagName === 'UL'){
+              for(let n = 0; n < navButtons.childNodes[l].childNodes.length; n++){
+                if(navButtons.childNodes[l].childNodes[n].tagName === 'LI'){
+                  let currentLi = navButtons.childNodes[l].childNodes[n];
+                  if(currentLi.innerHTML.includes(navButtonNumber)){
+                    currentLi.innerHTML = currentLi.innerHTML.slice(0, 6)
+                    currentLi.innerHTML = currentLi.innerHTML + ' Current ';
+                    if(!currentLi.className.includes('grey-color-button')){
+                      currentLi.className += 'grey-color-button'; 
+                    }
+                  } else {
+                    removeClass(currentLi, 'grey-color-button');
+                    if(currentLi.innerHTML.includes('Current')){
+                      currentLi.innerHTML = currentLi.innerHTML.slice(0, 6)
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
     } 
   })
 
-  let NavButtons = document.getElementById('nav-buttons');
-  NavButtons.addEventListener('click', (event)=>{
+  // nav button click event
+  let navButtons = document.getElementById('nav-buttons');
+  navButtons.addEventListener('click', (event)=>{
+    let articleNumber = event.srcElement.innerHTML.slice(5);
+    let articleNames = document.getElementsByTagName('article');
     event.preventDefault();
-    if(event.srcElement.tagName === 'A'){
-      let articleNumber = event.srcElement.innerHTML.slice(5);
-      let articleNames = document.getElementsByTagName('article');
+    if(event.srcElement.tagName === 'LI'){
       for(let i = 0; i< articleNames.length; i++){
         if(articleNames[i].id.includes(articleNumber)){
           // should take out no-show if there is a class
@@ -55,9 +88,7 @@ window.onload = ()=> {
           if(articleNames[i].className.includes('show')){
             // first check if element is currently being showed
             // dont do anything
-            console.log('clicked on the same as ths showing')
           } else {
-            console.log('clicked now the same as showing')
             for(let j = 0; j< articleNames.length; j++){
               if(articleNames[j].className.includes('show')){
                 removeClass(articleNames[j], 'show');
@@ -66,6 +97,37 @@ window.onload = ()=> {
             }
             removeClass(articleNames[i], 'hide');
             articleNames[i].className += ' show';
+          }
+        }
+      }
+    }
+    // changing the text of the nav 
+    if(event.srcElement){
+      // first find which article is beign displayed
+      for(let j = 0; j< articleNames.length; j++){
+        if(articleNames[j].className.includes('show')){
+          let navButtonNumber = j + 1;
+          // then find the nav li that has that navButtonNumber
+          for(let l = 0; l< navButtons.childNodes.length; l++){
+            if(navButtons.childNodes[l].tagName === 'UL'){
+              for(let n = 0; n < navButtons.childNodes[l].childNodes.length; n++){
+                if(navButtons.childNodes[l].childNodes[n].tagName === 'LI'){
+                  let currentLi = navButtons.childNodes[l].childNodes[n];
+                  if(currentLi.innerHTML.includes(navButtonNumber)){
+                    currentLi.innerHTML = currentLi.innerHTML.slice(0, 6)
+                    currentLi.innerHTML = currentLi.innerHTML + ' Current ';
+                    if(!currentLi.className.includes('grey-color-button')){
+                      currentLi.className += 'grey-color-button'; 
+                    }
+                  } else {
+                    removeClass(currentLi, 'grey-color-button');
+                    if(currentLi.innerHTML.includes('Current')){
+                      currentLi.innerHTML = currentLi.innerHTML.slice(0, 6)
+                    }
+                  }
+                }
+              }
+            }
           }
         }
       }
